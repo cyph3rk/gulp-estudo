f326637af187f69dfb6d7f877bd7e6558a28ed70
Diego Vargas <cyph3rk@gmail.com>
2022-03-13 00:07:07 -0300

    Finalizado projeto

--- README.md
+++ README.md
@@ -1,10 +1,41 @@
 
-NodeJs = 17.7.1
-NPM: 8.5.2
+Migração para versão mais atual (13/03/2022) do projeto do curso Alura: Gulp: automação de tarefas front-end
+https://cursos.alura.com.br/course/gulp
+
+
+Versão do NodeJs = 17.7.1
+
+Versão do NPM: 8.5.2
 
 gulp: 4.0.2 (https://www.npmjs.com/package/gulp)
+
 gulp-clean: 0.4.0  -> Apagar arquivos (https://www.npmjs.com/package/gulp-clean)
+
 gulp-concat: 2.6.1 -> Concatena arquivos (https://www.npmjs.com/package/gulp-concat)
+
 gulp-imagemin: 7.1.0 -> Otimiza imagens (https://www.npmjs.com/package/gulp-imagemin)
+
 gulp-uglify: 3.0.2 -> Coloca o arquivo em uma unica linha (https://www.npmjs.com/package/gulp-uglify)
+
 gulp-html-replace: 1.6.2  -> Substitui tags dentro de arquivos html (https://www.npmjs.com/package/gulp-html-replace)
+
+gulp-usemin: 0.3.30  -> Esse faz quase o mesmo que o gulp-html-replace, só que mais pratico pq faz tudo (https://www.npmjs.com/package/gulp-usemin)
+
+gulp-cssmin: 0.2.0 -> Minimaliza arquivos CSS (https://www.npmjs.com/package/gulp-cssmin)
+
+browser-sync: 2.27.7 -> Mini servidor para testar a pagina (https://browsersync.io/)
+
+gulp-jshint: 2.1.0  -> Hint de arquivos javascript.
+jshint: 2.13.4      ->    Que ajuda a indentificar erros no código
+
+jshint-stylish: 2.2.1 -> Melhora a saida do gulp-jshint (https://www.npmjs.com/package/jshint-stylish)
+
+gulp-csslint: 1.0.1  -> Faz o lint de arquivos CSS (https://www.npmjs.com/package/gulp-csslint)
+
+OBS: Migrei QUASE tudo. 
+Faltou as partes de: csslint:gulp-csslint / autoprefixer:gulp-autoprefixer / less:gulp-less.
+
+
+Para instalado: npm install <br>
+Roda as rotinas gulp: npm run gulp <br>
+Roda o Servidor: npm gulp servidor <br>
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
-const { series, parallel, src, dest } = require('gulp');
+const { watch, series, parallel, src, dest } = require('gulp');
 const clean = require('gulp-clean');
 const imagemin = require('gulp-imagemin');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const htmlReplace = require('gulp-html-replace');
+const usemin = require('gulp-usemin');
+const cssmin = require('gulp-cssmin');
+const browserSync = require("browser-sync").create();
+const jshint = require('gulp-jshint');
+const jshintStylesh = require('jshint-stylish');
 
 function copia () {
     return src('src/**/*').pipe(dest('dist'));    
@@ -19,6 +24,33 @@ function buildImg () {
         .pipe(dest('dist/img'));
 };
 
+// Essa funcao faz o mesmo q buildJs() e buildHtml ()
+function useMin () {
+    return src('dist/**/*.html')
+        .pipe(usemin({
+            'js' : [uglify],
+            'css' : [cssmin]
+        }))
+        .pipe(dest('dist'));
+};
+
+function servidor () {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+
+    watch('src/js/*.js', { events: 'all' }, function(path, stats)  {        
+        src(path)
+            .pipe(jshint())
+            .pipe(jshint.reporter('jshintStylesh'));
+    });    
+
+    watch('src/**/*' ).on('change', browserSync.reload);
+};
+
+// Não é mais usada nesse código, mas deichei aqui para estudo
 function buildJs () {
     const arq = ['dist/js/jquery.js', 'dist/js/home.js', 'dist/js/produto.js'];
     return src(arq).pipe(concat('all.js'))
@@ -26,6 +58,7 @@ function buildJs () {
            .pipe(dest('dist/js'));
 };
 
+// Não é mais usada nesse código, mas deichei aqui para estudo
 function buildHtml () {
     return src('dist/**/*.html')
         .pipe(htmlReplace( {
@@ -34,5 +67,6 @@ function buildHtml () {
         .pipe(dest('dist'));    
 };
 
-exports.default = series(apaga, copia, parallel(buildImg, buildJs), buildHtml);
+exports.servidor = servidor;
+exports.default = series(apaga, copia, parallel(buildImg, useMin));
 
