275b7acbe60047ce8c047541f9ac9e5dab829e6c
Diego Vargas <cyph3rk@gmail.com>
2022-03-12 22:34:00 -0300

    metade projeto migrado

---
+++ README.md
@@ -0,0 +1,10 @@
+
+NodeJs = 17.7.1
+NPM: 8.5.2
+
+gulp: 4.0.2 (https://www.npmjs.com/package/gulp)
+gulp-clean: 0.4.0  -> Apagar arquivos (https://www.npmjs.com/package/gulp-clean)
+gulp-concat: 2.6.1 -> Concatena arquivos (https://www.npmjs.com/package/gulp-concat)
+gulp-imagemin: 7.1.0 -> Otimiza imagens (https://www.npmjs.com/package/gulp-imagemin)
+gulp-uglify: 3.0.2 -> Coloca o arquivo em uma unica linha (https://www.npmjs.com/package/gulp-uglify)
+gulp-html-replace: 1.6.2  -> Substitui tags dentro de arquivos html (https://www.npmjs.com/package/gulp-html-replace)
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,38 @@
-const gulp = require('gulp');
+const { series, parallel, src, dest } = require('gulp');
+const clean = require('gulp-clean');
 const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const htmlReplace = require('gulp-html-replace');
 
-// gulp.task('copia', () => {
-//     gulp.src('src/**/*')
-//         .pipe(gulp.dest('dist'));
-// })
+function copia () {
+    return src('src/**/*').pipe(dest('dist'));    
+};
 
-gulp.task('build-img', function() {
-    gulp.src('dist/img/**/*')
+function apaga () {
+    return src('dist').pipe(clean());
+};
+
+function buildImg () {
+    return src('dist/img/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/img'));
-});
+        .pipe(dest('dist/img'));
+};
+
+function buildJs () {
+    const arq = ['dist/js/jquery.js', 'dist/js/home.js', 'dist/js/produto.js'];
+    return src(arq).pipe(concat('all.js'))
+           .pipe(uglify())
+           .pipe(dest('dist/js'));
+};
+
+function buildHtml () {
+    return src('dist/**/*.html')
+        .pipe(htmlReplace( {
+            js: 'js/all.js'
+        } ))
+        .pipe(dest('dist'));    
+};
+
+exports.default = series(apaga, copia, parallel(buildImg, buildJs), buildHtml);
+
